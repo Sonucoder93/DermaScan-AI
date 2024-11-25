b14453c24a4ebf888fc813ebefde78cd7b8fb2ba
Sonucoder93 <123187106+Sonucoder93@users.noreply.github.com>
2024-11-25 22:48:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRn36_T-L1qFjvDHk1ha-dxaQuw38rRMY",
+    authDomain: "fir-login-signup-master.firebaseapp.com",
+    projectId: "fir-login-signup-master",
+    storageBucket: "fir-login-signup-master.firebasestorage.app",
+    messagingSenderId: "777110320642",
+    appId: "1:777110320642:web:e59541f18a86d442b9191b",
+    measurementId: "G-GXF84BMGNK"
+  };
+export {firebaseConfig};
